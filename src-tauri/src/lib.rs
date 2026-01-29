d85b6261566a6773c89ae9e6b8246a093e49ab28
remi <remi.petit93370@gmail.com>
2026-01-29 22:49:38 +0100

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,23 @@
+use tauri::Manager;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn set_ignore_cursor_events(window: tauri::Window, ignore: bool) -> Result<(), String> {
+    window.set_ignore_cursor_events(ignore)
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, set_ignore_cursor_events])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
