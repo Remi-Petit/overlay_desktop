6b6d73051f33ddfbcc64d491af2c3c8c85203409
REMI PETIT <remi.petit@atos.net>
2026-01-30 15:40:44 +0100

    même niveau que la version vue

---
+++ app/pages/page2.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1>C'est la Page 2</h1>
+    <p>Le contenu de la deuxième section.</p>
+    
+    <NuxtLink to="/">
+      <button>Retourner à l'accueil</button>
+    </NuxtLink>
+  </main>
+</template>
